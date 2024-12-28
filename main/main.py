bb15e120c3cef204726a4b113bddc9381510f27d
Akram <mazakram04@gmail.com>
2024-12-28 00:02:29 -0500

    update

--- main/main.py
+++ main/main.py
@@ -1,2 +1,13 @@
 print("hello, world")
 print (17+35*2)
+
+nom = "Mazegh"
+prénom = "Akram"
+age = 22
+print(f" boujour je m'appelle {prénom} {nom} et j'ai {age} ans.")
+print(" Boujour je m'appelle " + prénom + " " + nom + " et j'ai " + str(age) + " age" )
+
+
+est_etudiant = True
+print(type(est_etudiant))
+print(type(age))
