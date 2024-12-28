401ef412ee12d88edf543dd1406fef0090b9f1b8
Akram <mazakram04@gmail.com>
2024-12-28 01:31:31 -0500

    update

--- main/main.py
+++ main/main.py
@@ -11,3 +11,24 @@ print(" Boujour je m'appelle " + prénom + " " + nom + " et j'ai " + str(age) + 
 est_etudiant = True
 print(type(est_etudiant))
 print(type(age))
+
+dragon_ball =["goku", "vegeta", "gohan", "trunks"]
+dragon_ball[3]="goten"
+dragon_ball.append("trunks")
+dragon_ball.sort()
+print(dragon_ball)
+
+nbr =[1,2,5,6,"ak"]
+print ("ak" in nbr)
+
+fruits = ["pomme","banane", "orange"]
+fruits.append ("kiwi")
+print(fruits)
+
+del fruits[2]
+print(fruits)
+
+print(len(fruits))
+
+fruits.sort()
+print(fruits)
